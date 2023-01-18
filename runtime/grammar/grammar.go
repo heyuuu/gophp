c6f168d6c29222f1c66b246ab89c86865d4ea75f
He Yu <code@qheyu.com>
2023-01-18 13:30:04 +0800

    添加runtime包

---
+++ runtime/grammar/grammar.go
@@ -0,0 +1,75 @@
+package grammar
+
+type num interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+func Assign[T any](variable *T, value T) T {
+	*variable = value
+	return *variable
+}
+
+func AssignOp[T any](variable *T, value T, op string) T {
+	// todo 待处理各 op 类型
+	*variable = value
+	return *variable
+}
+
+func Cond[T any](cond bool, trueValue T, falseValue T) T {
+	if cond {
+		return trueValue
+	}
+	return falseValue
+}
+
+func CondF[T any](cond bool, trueValue func() T, falseValue func() T) T {
+	if cond {
+		return trueValue()
+	}
+	return falseValue()
+}
+
+func CondF1[T any](cond bool, trueValue func() T, falseValue T) T {
+	if cond {
+		return trueValue()
+	}
+	return falseValue
+}
+
+func CondF2[T any](cond bool, trueValue T, falseValue func() T) T {
+	if cond {
+		return trueValue
+	}
+	return falseValue()
+}
+
+func Goto(label any) {
+	// todo
+}
+
+func SizeOf() int {
+	// todo
+	return 0
+}
+
+func PreInc[T num](variable *T) T {
+	*variable++
+	return *variable
+}
+
+func PreDec[T num](variable *T) T {
+	*variable--
+	return *variable
+}
+
+func PostInc[T num](variable *T) T {
+	var result = *variable
+	*variable++
+	return result
+}
+
+func PostDec[T num](variable *T) T {
+	var result = *variable
+	*variable--
+	return result
+}
