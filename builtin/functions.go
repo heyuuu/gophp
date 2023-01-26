f99e9bf8b59ae585f5cf195fbfd7e459b3546ab6
He Yu <code@qheyu.com>
2023-01-26 15:39:43 +0800

    WIP: builtin

--- builtin/functions.go
+++ builtin/functions.go
@@ -1,8 +1,14 @@
 package builtin
 
+import "unsafe"
+
 type integer interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 func FlagMatch[T integer](flags T, flag T) bool { return flags&flag != 0 }
 func FlagMatchNum[T integer](flags T, flag T) T { return flags & flag }
+
+func ForceCastPtr[T any, N integer](ptr N) *T {
+	return (*T)(unsafe.Pointer(uintptr(ptr)))
+}
