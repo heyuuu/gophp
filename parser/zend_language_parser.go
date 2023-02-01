ce51529d98b2647c5aeb586e708183960047f5f5
He Yu <code@qheyu.com>
2023-02-01 21:41:45 +0800

    init zend_language 的 lexer 和 parser

---
+++ parser/zend_language_parser.go
@@ -0,0 +1,3 @@
+package zend
+
+//go:generate goyacc -o ../zend/zend_language_parser.go -p "Zend" zend_language_parser.y
---
+++ parser/zend_language_scanner.go
@@ -0,0 +1,3 @@
+package zend
+
+//go:generate re2go zend_language_scanner.l --case-inverted --no-generation-date -cbdFt ../zend/zend_language_scanner.defs._.go -o ../zend/zend_language_scanner.go -i
