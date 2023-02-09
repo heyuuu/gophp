4406041a6dd9defcdac553343dea8c6003b72749
He Yu <code@qheyu.com>
2023-02-09 16:56:46 +0800

    WIP

--- parser/zend_language_scanner.go
+++ parser/zend_language_scanner.go
@@ -1,3 +1,3 @@
 package zend
 
-//go:generate re2go zend_language_scanner.l --case-inverted --no-generation-date -cbdFt ../zend/zend_language_scanner.defs._.go -o ../zend/zend_language_scanner.go -i
+//go:generate re2go zend_language_scanner.l --case-inverted --no-generation-date -cbdFt ../zend/zend_language_scanner_defs._.go -o ../zend/zend_language_scanner_gen.go -i
