146dad71c4f77b06866e549a52380281bdc0320a
He Yu <code@qheyu.com>
2023-02-16 15:34:44 +0800

    脚本，替换 MakeFunctionEntry

--- script/replace/replace_make_function_def.go
+++ script/replace/replace_make_function_def.go
@@ -9,7 +9,7 @@ import (
 
 func replaceMakeFunctionEntryDef(code string) string {
 	rule := regexp.MustCompile(`(zend\.)?MakeZendFunctionEntry\((.*)\),`)
-	sizeRule := regexp.MustCompile(`uint32\(b\.SizeOf\("(arginfo_zend__void)"\)/b.SizeOf\("struct _zend_internal_arg_info"\)-1\)`)
+	sizeRule := regexp.MustCompile(`uint32\(b\.SizeOf\("(\w+)"\)/b.SizeOf\("struct _zend_internal_arg_info"\)-1\)`)
 
 	result := util.RegexReplaceAll(rule, code, func(matches []string) string {
 		pkgPrefix := matches[1]
@@ -25,13 +25,30 @@ func replaceMakeFunctionEntryDef(code string) string {
 		num_args := args[3]
 		flags := args[4]
 
-		sizeMatches := sizeRule.FindStringSubmatch(num_args)
-		if len(sizeMatches) == 0 || strutil.UpperCamelCase(sizeMatches[1]) != arg_info {
-			return matches[0]
+		if !(arg_info == "nil" && num_args == "0") {
+			sizeMatches := sizeRule.FindStringSubmatch(num_args)
+			if len(sizeMatches) < 2 || !isSameName(arg_info, sizeMatches[1]) {
+				fmt.Println([]string{arg_info, num_args})
+				fmt.Println(sizeMatches)
+				return matches[0]
+			}
 		}
 
-		return fmt.Sprintf(`%sMakeZendFunctionEntry(%s, %s, %s, %s),`, pkgPrefix, name, flags, handler, arg_info)
+		return fmt.Sprintf(`%sMakeZendFunctionEntryEx(%s, %s, %s, %s),`, pkgPrefix, name, flags, handler, arg_info)
 	})
 
 	return result
+}
+
+func isSameName(name string, rawName string) bool {
+	if name == "nil" && rawName == "NULL" {
+		return true
+	}
+	if name == rawName {
+		return true
+	}
+	if name == strutil.UpperCamelCase(rawName) {
+		return true
+	}
+	return false
 }
--- script/replace/replace_make_function_def_test.go
+++ script/replace/replace_make_function_def_test.go
@@ -1,8 +1,23 @@
 package replace
 
-import "testing"
+import (
+	"sik/script/util"
+	"sik/script/util/finder"
+	"testing"
+)
 
 func Test_replaceMakeFunctionEntryDef(t *testing.T) {
+	f := finder.DefaultProjectFinder()
+	f.Walk(func(file finder.FileInfo) {
+		code := util.MustReadFileString(file.Path)
+		result := replaceMakeFunctionEntryDef(code)
+		if result != code {
+			util.MustWriteFileString(file.Path, result)
+		}
+	})
+}
+
+func Test_replaceMakeFunctionEntryDef_Dev(t *testing.T) {
 	code := `
 var BuiltinFunctions []ZendFunctionEntry = []ZendFunctionEntry{
 	MakeZendFunctionEntry("zend_version", ZifZendVersion, ArginfoZendVoid, uint32(b.SizeOf("arginfo_zend__void")/b.SizeOf("struct _zend_internal_arg_info")-1), 0),
