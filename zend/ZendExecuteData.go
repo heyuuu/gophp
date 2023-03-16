2b5c8c63ddb68a3a2f11b3bdb2086e6cd4e19469
He Yu <code@qheyu.com>
2023-03-16 18:04:08 +0800

    调整 ZendExecuteData 参数个数相关方法

--- zend/ZendExecuteData.go
+++ zend/ZendExecuteData.go
@@ -8,38 +8,32 @@ import b "sik/builtin"
  * ZendExecuteData
  */
 type ZendExecuteData struct {
-	opline            *ZendOp
-	call              *ZendExecuteData
-	return_value      *Zval
-	func_             *ZendFunction
-	This              Zval
-	prev_execute_data *ZendExecuteData
-	symbol_table      *ZendArray
-	run_time_cache    *any
+	opline          *ZendOp
+	call            *ZendExecuteData
+	return_value    *Zval
+	func_           *ZendFunction
+	This            Zval
+	prevExecuteData *ZendExecuteData
+	symbol_table    *ZendArray
+	run_time_cache  *any
 }
 
 func (this *ZendExecuteData) isStrictTypes() bool {
 	return this != nil && this.func_ != nil && this.func_.IsStrictTypes()
 }
-func (this *ZendExecuteData) IsStrictTypesCall() bool {
-	return this.isStrictTypes()
-}
-func (this *ZendExecuteData) IsStrictTypesArg() bool {
-	return this.prev_execute_data.isStrictTypes()
-}
-func (this *ZendExecuteData) IsStrictTypesRet() bool {
-	return this.isStrictTypes()
-}
+func (this *ZendExecuteData) IsCallUseStrictTypes() bool { return this.isStrictTypes() }
+func (this *ZendExecuteData) IsRetUseStrictTypes() bool  { return this.isStrictTypes() }
+func (this *ZendExecuteData) IsArgUseStrictTypes() bool  { return this.prevExecuteData.isStrictTypes() }
 
 func (this *ZendExecuteData) NumArgs() uint32 { return this.This.GetNumArgs() }
 
 func (this *ZendExecuteData) CheckNumArgsError(minNumArgs int, maxNumArgs int) bool {
-	return this.CheckNumArgs(minNumArgs, maxNumArgs, ZEND_ARG_USES_STRICT_TYPES())
+	return this.CheckNumArgs(minNumArgs, maxNumArgs, false)
 }
 func (this *ZendExecuteData) CheckNumArgsException(minNumArgs int, maxNumArgs int) bool {
 	return this.CheckNumArgs(minNumArgs, maxNumArgs, true)
 }
-func (this *ZendExecuteData) CheckNumArgs(minNumArgs int, maxNumArgs int, throwException bool) bool {
+func (this *ZendExecuteData) CheckNumArgs(minNumArgs int, maxNumArgs int, forceStrict bool) bool {
 	// 检查参数个数，若检查通过直接返回
 	numArgs := int(this.NumArgs())
 	if numArgs >= minNumArgs && numArgs <= maxNumArgs {
@@ -56,6 +50,8 @@ func (this *ZendExecuteData) CheckNumArgs(minNumArgs int, maxNumArgs int, throwE
 		callee = activeFunc.GetFunctionName().GetStr()
 	}
 
+	// 判断是否为 strict 模式
+	var throwException = forceStrict || this.IsArgUseStrictTypes()
 	if minNumArgs == maxNumArgs {
 		ZendInternalArgumentCountError(throwException, "%s() expects exactly %d parameter%s, %d given", callee, minNumArgs, b.Cond(minNumArgs == 1, "", "s"), numArgs)
 	} else if numArgs < minNumArgs {
@@ -77,9 +73,9 @@ func (this *ZendExecuteData) SetReturnValue(value *Zval)           { this.return
 func (this *ZendExecuteData) GetFunc() *ZendFunction               { return this.func_ }
 func (this *ZendExecuteData) SetFunc(value *ZendFunction)          { this.func_ = value }
 func (this *ZendExecuteData) GetThis() *Zval                       { return &this.This }
-func (this *ZendExecuteData) GetPrevExecuteData() *ZendExecuteData { return this.prev_execute_data }
+func (this *ZendExecuteData) GetPrevExecuteData() *ZendExecuteData { return this.prevExecuteData }
 func (this *ZendExecuteData) SetPrevExecuteData(value *ZendExecuteData) {
-	this.prev_execute_data = value
+	this.prevExecuteData = value
 }
 func (this *ZendExecuteData) GetSymbolTable() *ZendArray      { return this.symbol_table }
 func (this *ZendExecuteData) SetSymbolTable(value *ZendArray) { this.symbol_table = value }
