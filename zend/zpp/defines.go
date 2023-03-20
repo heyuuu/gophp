e34b265a39ad953e31eb464ce1415d9aa832de75
He Yu <code@qheyu.com>
2023-03-20 14:37:57 +0800

    定义 Zif 生成所需类型参数，添加文档

--- zend/zpp/defines.go
+++ zend/zpp/defines.go
@@ -1,1 +1,17 @@
 package zpp
+
+import (
+	"sik/zend"
+	"sik/zend/types"
+)
+
+/**
+ * 此处的类型用于 sikgen 脚本生成代码
+ */
+type (
+	DefOpt any
+	DefEx  *zend.ZendExecuteData
+	DefRet *types.Zval
+	// Type: 'L', FAST_ZPP: Z_PARAM_STRICT_LONG
+	DefStrictLong = int
+)
--- zend/zpp/fast_zpp.go
+++ zend/zpp/fast_zpp.go
@@ -8,38 +8,7 @@ import (
 /**
  * FAST_ZPP: PHP7之后新增的参数处理方式
  * @link: https://wiki.php.net/rfc/fast_zpp
- * 涉及对应以下宏
- * - ZEND_PARSE_PARAMETERS_START
- * - ZEND_PARSE_PARAMETERS_END
- * - ZEND_PARAM_OPTION
- * - ZEND_PARSE_*
- * - ...
- *
- * FAST_ZPP 宏与原描述符对应表 (@se):
- * | 原描述符 |  		FAST_ZPP 宏 			| 					参数类型 							|
- * | ------ | --------------------------------- | ----------------------------------------------------- |
- * | 	| 	| Z_PARAM_OPTIONAL 					| 														|
- * | 	a 	| Z_PARAM_ARRAY(dest) 				| dest - zval* 											|
- * | 	A 	| Z_PARAM_ARRAY_OR_OBJECT(dest) 	| dest - zval* 											|
- * | 	b 	| Z_PARAM_BOOL(dest) 				| dest - zend_bool 										|
- * | 	C 	| Z_PARAM_CLASS(dest) 				| dest - zend_class_entry* 								|
- * | 	d 	| Z_PARAM_DOUBLE(dest) 				| dest - double 										|
- * | 	f 	| Z_PARAM_FUNC(fci, fcc) 			| fci - zend_fcall_info, fcc - zend_fcall_info_cache 	|
- * | 	h 	| Z_PARAM_ARRAY_HT(dest) 			| dest - HashTable* 									|
- * | 	H 	| Z_PARAM_ARRAY_OR_OBJECT_HT(dest) 	| dest - HashTable* 									|
- * | 	l 	| Z_PARAM_LONG(dest) 				| dest - long 											|
- * | 	L 	| Z_PARAM_STRICT_LONG(dest) 		| dest - long 											|
- * | 	o 	| Z_PARAM_OBJECT(dest) 				| dest - zval* 											|
- * | 	O 	| Z_PARAM_OBJECT_OF_CLASS(dest, ce) | dest - zval* 											|
- * | 	p 	| Z_PARAM_PATH(dest, dest_len) 		| dest - char*, dest_len - int 							|
- * | 	P 	| Z_PARAM_PATH_STR(dest) 			| dest - zend_string* 									|
- * | 	r 	| Z_PARAM_RESOURCE(dest) 			| dest - zval* 											|
- * | 	s 	| Z_PARAM_STRING(dest, dest_len) 	| dest - char*, dest_len - int 							|
- * | 	S 	| Z_PARAM_STR(dest) 				| dest - zend_string* 									|
- * | 	z 	| Z_PARAM_ZVAL(dest) 				| dest - zval* 											|
- * | 	  	| Z_PARAM_ZVAL_DEREF(dest) 			| dest - zval* 											|
- * | 	+ 	| Z_PARAM_VARIADIC('+', dest, num) 	| dest - zval*, num int 								|
- * | 	* 	| Z_PARAM_VARIADIC('*', dest, num) 	| dest - zval*, num int 								|
+ * FAST_ZPP 宏与原描述符对应表 (@see README.md):
  */
 type FastParser struct {
 	baseParser
