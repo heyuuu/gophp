f419bf96012998bda74d0ad57e7de4c801f94bd6
HeYu <code@qheyu.com>
2023-03-26 16:07:27 +0800

    WIP

--- core/streams/userspace.types.go
+++ core/streams/userspace.types.go
@@ -15,24 +15,12 @@ type PhpUserStreamWrapper struct {
 	wrapper   core.PhpStreamWrapper
 }
 
-// func MakePhpUserStreamWrapper(protoname *byte, classname *byte, ce *zend.ClassEntry, wrapper core.PhpStreamWrapper) PhpUserStreamWrapper {
-//     return PhpUserStreamWrapper{
-//         protoname:protoname,
-//         classname:classname,
-//         ce:ce,
-//         wrapper:wrapper,
-//     }
-// }
-func (this *PhpUserStreamWrapper) GetProtoname() *byte      { return this.protoname }
-func (this *PhpUserStreamWrapper) SetProtoname(value *byte) { this.protoname = value }
-func (this *PhpUserStreamWrapper) GetClassname() *byte      { return this.classname }
-func (this *PhpUserStreamWrapper) SetClassname(value *byte) { this.classname = value }
-func (this *PhpUserStreamWrapper) GetCe() *types.ClassEntry { return this.ce }
-
-// func (this *PhpUserStreamWrapper) SetCe(value *zend.ClassEntry) { this.ce = value }
+func (this *PhpUserStreamWrapper) GetProtoname() *byte               { return this.protoname }
+func (this *PhpUserStreamWrapper) SetProtoname(value *byte)          { this.protoname = value }
+func (this *PhpUserStreamWrapper) GetClassname() *byte               { return this.classname }
+func (this *PhpUserStreamWrapper) SetClassname(value *byte)          { this.classname = value }
+func (this *PhpUserStreamWrapper) GetCe() *types.ClassEntry          { return this.ce }
 func (this *PhpUserStreamWrapper) GetWrapper() core.PhpStreamWrapper { return this.wrapper }
-
-// func (this *PhpUserStreamWrapper) SetWrapper(value core.PhpStreamWrapper) { this.wrapper = value }
 
 /**
  * _phpUserstreamData
@@ -42,14 +30,6 @@ type _phpUserstreamData struct {
 	object  types.Zval
 }
 
-// func Make_phpUserstreamData(wrapper *PhpUserStreamWrapper, object zend.Zval) _phpUserstreamData {
-//     return _phpUserstreamData{
-//         wrapper:wrapper,
-//         object:object,
-//     }
-// }
 func (this *_phpUserstreamData) GetWrapper() *PhpUserStreamWrapper      { return this.wrapper }
 func (this *_phpUserstreamData) SetWrapper(value *PhpUserStreamWrapper) { this.wrapper = value }
 func (this *_phpUserstreamData) GetObject() types.Zval                  { return this.object }
-
-// func (this *_phpUserstreamData) SetObject(value zend.Zval) { this.object = value }
