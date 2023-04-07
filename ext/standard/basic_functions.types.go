bd8ad9e08605ff2aad36851b6339faacdf3493be
HeYu <code@qheyu.com>
2023-04-07 21:33:41 +0800

    clean code

--- ext/standard/basic_functions.types.go
+++ ext/standard/basic_functions.types.go
@@ -61,106 +61,18 @@ type PhpBasicGlobals struct {
 	unserialize_max_depth      zend.ZendLong
 }
 
-// func (this *PhpBasicGlobals)  GetUserShutdownFunctionNames() *zend.HashTable      { return this.user_shutdown_function_names }
-// func (this *PhpBasicGlobals) SetUserShutdownFunctionNames(value *zend.HashTable) { this.user_shutdown_function_names = value }
-// func (this *PhpBasicGlobals)  GetPutenvHt() zend.HashTable      { return this.putenv_ht }
-// func (this *PhpBasicGlobals) SetPutenvHt(value zend.HashTable) { this.putenv_ht = value }
-// func (this *PhpBasicGlobals)  GetStrtokZval() zend.Zval      { return this.strtok_zval }
-// func (this *PhpBasicGlobals) SetStrtokZval(value zend.Zval) { this.strtok_zval = value }
-// func (this *PhpBasicGlobals)  GetStrtokString() *byte      { return this.strtok_string }
-// func (this *PhpBasicGlobals) SetStrtokString(value *byte) { this.strtok_string = value }
-// func (this *PhpBasicGlobals)  GetLocaleString() *zend.String      { return this.locale_string }
-// func (this *PhpBasicGlobals) SetLocaleString(value *zend.String) { this.locale_string = value }
-// func (this *PhpBasicGlobals)  GetLocaleChanged() zend.ZendBool      { return this.locale_changed }
-// func (this *PhpBasicGlobals) SetLocaleChanged(value zend.ZendBool) { this.locale_changed = value }
-// func (this *PhpBasicGlobals)  GetStrtokLast() *byte      { return this.strtok_last }
-// func (this *PhpBasicGlobals) SetStrtokLast(value *byte) { this.strtok_last = value }
-// func (this *PhpBasicGlobals)  GetStrtokTable() []byte      { return this.strtok_table }
-// func (this *PhpBasicGlobals) SetStrtokTable(value []byte) { this.strtok_table = value }
-// func (this *PhpBasicGlobals)  GetStrtokLen() zend.ZendUlong      { return this.strtok_len }
-// func (this *PhpBasicGlobals) SetStrtokLen(value zend.ZendUlong) { this.strtok_len = value }
-// func (this *PhpBasicGlobals)  GetStrEbuf() []byte      { return this.str_ebuf }
-// func (this *PhpBasicGlobals) SetStrEbuf(value []byte) { this.str_ebuf = value }
-// func (this *PhpBasicGlobals)  GetArrayWalkFci() zend.ZendFcallInfo      { return this.array_walk_fci }
-// func (this *PhpBasicGlobals) SetArrayWalkFci(value zend.ZendFcallInfo) { this.array_walk_fci = value }
-// func (this *PhpBasicGlobals)  GetArrayWalkFciCache() zend.ZendFcallInfoCache      { return this.array_walk_fci_cache }
-// func (this *PhpBasicGlobals) SetArrayWalkFciCache(value zend.ZendFcallInfoCache) { this.array_walk_fci_cache = value }
-// func (this *PhpBasicGlobals)  GetUserCompareFci() zend.ZendFcallInfo      { return this.user_compare_fci }
-// func (this *PhpBasicGlobals) SetUserCompareFci(value zend.ZendFcallInfo) { this.user_compare_fci = value }
-// func (this *PhpBasicGlobals)  GetUserCompareFciCache() zend.ZendFcallInfoCache      { return this.user_compare_fci_cache }
-// func (this *PhpBasicGlobals) SetUserCompareFciCache(value zend.ZendFcallInfoCache) { this.user_compare_fci_cache = value }
-// func (this *PhpBasicGlobals)  GetUserTickFunctions() *zend.ZendLlist      { return this.user_tick_functions }
-// func (this *PhpBasicGlobals) SetUserTickFunctions(value *zend.ZendLlist) { this.user_tick_functions = value }
-// func (this *PhpBasicGlobals)  GetActiveIniFileSection() zend.Zval      { return this.active_ini_file_section }
-// func (this *PhpBasicGlobals) SetActiveIniFileSection(value zend.Zval) { this.active_ini_file_section = value }
-// func (this *PhpBasicGlobals)  GetPageUid() zend.ZendLong      { return this.page_uid }
-// func (this *PhpBasicGlobals) SetPageUid(value zend.ZendLong) { this.page_uid = value }
-// func (this *PhpBasicGlobals)  GetPageGid() zend.ZendLong      { return this.page_gid }
-// func (this *PhpBasicGlobals) SetPageGid(value zend.ZendLong) { this.page_gid = value }
-// func (this *PhpBasicGlobals)  GetPageInode() zend.ZendLong      { return this.page_inode }
-// func (this *PhpBasicGlobals) SetPageInode(value zend.ZendLong) { this.page_inode = value }
-// func (this *PhpBasicGlobals)  GetPageMtime() int64      { return this.page_mtime }
-// func (this *PhpBasicGlobals) SetPageMtime(value int64) { this.page_mtime = value }
-// func (this *PhpBasicGlobals)  GetCurrentStatFile() *byte      { return this.CurrentStatFile }
-// func (this *PhpBasicGlobals) SetCurrentStatFile(value *byte) { this.CurrentStatFile = value }
-// func (this *PhpBasicGlobals)  GetCurrentLStatFile() **byte      { return this.CurrentLStatFile }
-// func (this *PhpBasicGlobals) SetCurrentLStatFile(value **byte) { this.CurrentLStatFile = value }
-// func (this *PhpBasicGlobals)  GetSsb() core.PhpStreamStatbuf      { return this.ssb }
-// func (this *PhpBasicGlobals) SetSsb(value core.PhpStreamStatbuf) { this.ssb = value }
-// func (this *PhpBasicGlobals)  GetLssb() core.PhpStreamStatbuf      { return this.lssb }
-// func (this *PhpBasicGlobals) SetLssb(value core.PhpStreamStatbuf) { this.lssb = value }
-// func (this *PhpBasicGlobals)  GetState() []uint32      { return this.state }
-// func (this *PhpBasicGlobals) SetState(value []uint32) { this.state = value }
-// func (this *PhpBasicGlobals)  GetNext() *uint32      { return this.next }
-// func (this *PhpBasicGlobals) SetNext(value *uint32) { this.next = value }
-// func (this *PhpBasicGlobals)  GetLeft() int      { return this.left }
-// func (this *PhpBasicGlobals) SetLeft(value int) { this.left = value }
-// func (this *PhpBasicGlobals)  GetMtRandIsSeeded() zend.ZendBool      { return this.mt_rand_is_seeded }
-// func (this *PhpBasicGlobals) SetMtRandIsSeeded(value zend.ZendBool) { this.mt_rand_is_seeded = value }
-// func (this *PhpBasicGlobals)  GetMtRandMode() zend.ZendLong      { return this.mt_rand_mode }
-// func (this *PhpBasicGlobals) SetMtRandMode(value zend.ZendLong) { this.mt_rand_mode = value }
-// func (this *PhpBasicGlobals)  GetSyslogDevice() *byte      { return this.syslog_device }
-// func (this *PhpBasicGlobals) SetSyslogDevice(value *byte) { this.syslog_device = value }
-// func (this *PhpBasicGlobals)  GetIncompleteClass() *zend.ClassEntry      { return this.incomplete_class }
-// func (this *PhpBasicGlobals) SetIncompleteClass(value *zend.ClassEntry) { this.incomplete_class = value }
-// func (this *PhpBasicGlobals)  GetSerializeLock() unsigned      { return this.serialize_lock }
-// func (this *PhpBasicGlobals) SetSerializeLock(value unsigned) { this.serialize_lock = value }
-// func (this *PhpBasicGlobals)  GetSerializeData() *PhpSerializeData      { return this.serialize.data }
-// func (this *PhpBasicGlobals) SetSerializeData(value *PhpSerializeData) { this.serialize.data = value }
-// func (this *PhpBasicGlobals)  GetSerializeLevel() unsigned      { return this.serialize.level }
-// func (this *PhpBasicGlobals) SetSerializeLevel(value unsigned) { this.serialize.level = value }
-// func (this *PhpBasicGlobals)  GetUnserializeData() *PhpUnserializeData      { return this.unserialize.data }
-// func (this *PhpBasicGlobals) SetUnserializeData(value *PhpUnserializeData) { this.unserialize.data = value }
-// func (this *PhpBasicGlobals)  GetUnserializeLevel() unsigned      { return this.unserialize.level }
-// func (this *PhpBasicGlobals) SetUnserializeLevel(value unsigned) { this.unserialize.level = value }
 func (this *PhpBasicGlobals) GetUrlAdaptSessionEx() UrlAdaptStateExT {
 	return this.url_adapt_session_ex
 }
 
-// func (this *PhpBasicGlobals) SetUrlAdaptSessionEx(value UrlAdaptStateExT) { this.url_adapt_session_ex = value }
 func (this *PhpBasicGlobals) GetUrlAdaptSessionHostsHt() types.Array {
 	return this.url_adapt_session_hosts_ht
 }
 
-// func (this *PhpBasicGlobals) SetUrlAdaptSessionHostsHt(value zend.HashTable) { this.url_adapt_session_hosts_ht = value }
 func (this *PhpBasicGlobals) GetUrlAdaptOutputEx() UrlAdaptStateExT { return this.url_adapt_output_ex }
-
-// func (this *PhpBasicGlobals) SetUrlAdaptOutputEx(value UrlAdaptStateExT) { this.url_adapt_output_ex = value }
 func (this *PhpBasicGlobals) GetUrlAdaptOutputHostsHt() types.Array {
 	return this.url_adapt_output_hosts_ht
 }
-
-// func (this *PhpBasicGlobals) SetUrlAdaptOutputHostsHt(value zend.HashTable) { this.url_adapt_output_hosts_ht = value }
-// func (this *PhpBasicGlobals)  GetMmapFile() any      { return this.mmap_file }
-// func (this *PhpBasicGlobals) SetMmapFile(value any) { this.mmap_file = value }
-// func (this *PhpBasicGlobals)  GetMmapLen() int      { return this.mmap_len }
-// func (this *PhpBasicGlobals) SetMmapLen(value int) { this.mmap_len = value }
-// func (this *PhpBasicGlobals)  GetUserFilterMap() *zend.HashTable      { return this.user_filter_map }
-// func (this *PhpBasicGlobals) SetUserFilterMap(value *zend.HashTable) { this.user_filter_map = value }
-// func (this *PhpBasicGlobals)  GetUmask() int      { return this.umask }
-// func (this *PhpBasicGlobals) SetUmask(value int) { this.umask = value }
-// func (this *PhpBasicGlobals)  GetUnserializeMaxDepth() zend.ZendLong      { return this.unserialize_max_depth }
-// func (this *PhpBasicGlobals) SetUnserializeMaxDepth(value zend.ZendLong) { this.unserialize_max_depth = value }
 
 /**
  * PutenvEntry
@@ -172,14 +84,6 @@ type PutenvEntry struct {
 	key_len        int
 }
 
-// func MakePutenvEntry(putenv_string *byte, previous_value *byte, key *byte, key_len int) PutenvEntry {
-//     return PutenvEntry{
-//         putenv_string:putenv_string,
-//         previous_value:previous_value,
-//         key:key,
-//         key_len:key_len,
-//     }
-// }
 func (this *PutenvEntry) GetPutenvString() *byte       { return this.putenv_string }
 func (this *PutenvEntry) SetPutenvString(value *byte)  { this.putenv_string = value }
 func (this *PutenvEntry) GetPreviousValue() *byte      { return this.previous_value }
@@ -197,12 +101,6 @@ type PhpShutdownFunctionEntry struct {
 	arg_count int
 }
 
-// func MakePhpShutdownFunctionEntry(arguments *zend.Zval, arg_count int) PhpShutdownFunctionEntry {
-//     return PhpShutdownFunctionEntry{
-//         arguments:arguments,
-//         arg_count:arg_count,
-//     }
-// }
 func (this *PhpShutdownFunctionEntry) GetArguments() *types.Zval      { return this.arguments }
 func (this *PhpShutdownFunctionEntry) SetArguments(value *types.Zval) { this.arguments = value }
 func (this *PhpShutdownFunctionEntry) GetArgCount() int               { return this.arg_count }
@@ -217,13 +115,6 @@ type UserTickFunctionEntry struct {
 	calling   int
 }
 
-// func MakeUserTickFunctionEntry(arguments *zend.Zval, arg_count int, calling int) UserTickFunctionEntry {
-//     return UserTickFunctionEntry{
-//         arguments:arguments,
-//         arg_count:arg_count,
-//         calling:calling,
-//     }
-// }
 func (this *UserTickFunctionEntry) GetArguments() *types.Zval      { return this.arguments }
 func (this *UserTickFunctionEntry) SetArguments(value *types.Zval) { this.arguments = value }
 func (this *UserTickFunctionEntry) GetArgCount() int               { return this.arg_count }
