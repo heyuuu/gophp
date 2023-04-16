e907068dc7bf1908e9b9b047cb3f1115d2d7e8f5
HeYu <code@qheyu.com>
2023-04-16 16:45:04 +0800

    调整 ZEND_INIT_FCALL.op1.num 值含义，从需要的 ex 内存大小改成需要的 ex.runtimeCache 数组大小

--- zend/execute.go
+++ zend/execute.go
@@ -6,28 +6,19 @@ import (
 )
 
 func ZendVmStackPushCallFrame(callInfo uint32, func_ types.IFunction, numArgs uint32, objectOrCalledScope any) *ZendExecuteData {
-	var runtimeCacheSize uint32 = numArgs
-	if ZEND_USER_CODE(func_.GetType()) {
-		runtimeCacheSize += func_.GetOpArray().GetLastVar() + func_.GetOpArray().GetT() - b.Min(func_.GetOpArray().GetNumArgs(), numArgs)
-	}
+	var runtimeCacheSize uint32 = ZendVmCalcUsedStack(numArgs, func_)
 	return ZendVmStackPushCallFrameEx(runtimeCacheSize, callInfo, func_, numArgs, objectOrCalledScope)
 }
 
 func ZendVmCalcUsedStack(numArgs uint32, func_ types.IFunction) uint32 {
-	var usedStack uint32 = ZEND_CALL_FRAME_SLOT + numArgs
+	var usedStack = numArgs
 	if ZEND_USER_CODE(func_.GetType()) {
 		usedStack += func_.GetOpArray().GetLastVar() + func_.GetOpArray().GetT() - b.Min(func_.GetOpArray().GetNumArgs(), numArgs)
 	}
-	return usedStack * b.SizeOf("zval")
+	return usedStack
 }
 
-func ZendVmStackPushCallFrameEx(usedStack uint32, callInfo uint32, func_ types.IFunction, numArgs uint32, objectOrCalledScope any) *ZendExecuteData {
-	runtimeCacheSize := usedStack/b.SizeOf("zval") - ZEND_CALL_FRAME_SLOT
-	//callInfo |= ZEND_CALL_ALLOCATED
-	return ZendVmStackPushCallFrameExEx(runtimeCacheSize, callInfo, func_, numArgs, objectOrCalledScope)
-}
-
-func ZendVmStackPushCallFrameExEx(runtimeCacheSize uint32, callInfo uint32, func_ types.IFunction, numArgs uint32, objectOrCalledScope any) *ZendExecuteData {
+func ZendVmStackPushCallFrameEx(runtimeCacheSize uint32, callInfo uint32, func_ types.IFunction, numArgs uint32, objectOrCalledScope any) *ZendExecuteData {
 	var call *ZendExecuteData = NewExecuteData(callInfo, func_, numArgs, objectOrCalledScope, runtimeCacheSize)
 	EG__().VmStack().Push(call)
 	return call
