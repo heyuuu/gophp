ede6cacd9004d8ffb1bc8b72b33ff61d35c6f3bf
HeYu <code@qheyu.com>
2023-07-26 22:36:37 +0800

    delete unused files

--- parser/zend_language_scanner.go
+++
@@ -1,3 +0,0 @@
-package zend
-
-//go:generate re2go zend_language_scanner_gen.l --case-inverted --no-generation-date -cbFt ../zend/zend_language_scanner_defs._.go -o ../zend/zend_language_scanner_gen.go -i
