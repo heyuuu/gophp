64b451b2da274e6aa4be86f4fdd85f12d103508b
HeYu <code@qheyu.com>
2024-01-29 14:02:32 +0800

    fix bugs: 修复 base64_decode 在各种异常数据下和 php 行为不一致的问题

--- ext/standard/base64.go
+++ ext/standard/base64.go
@@ -6,43 +6,89 @@ import (
 	"strings"
 )
 
-const base64Table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+const base64padding = '='
+const base64Table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
 func PhpBase64Encode(str string) string {
 	return base64.StdEncoding.EncodeToString([]byte(str))
 }
-func PhpBase64DecodeEx(str string, strict bool) (string, bool) {
-	cleanStr, ok := base64CleanStr(str, strict)
-	if !ok {
+func PhpBase64Decode(str string, strict bool) (string, bool) {
+	if !strict {
+		str = base64CleanStrWeak(str)
+	} else if cleanStr, ok := base64CleanStrStrict(str); ok {
+		str = cleanStr
+	} else {
 		return "", false
 	}
 
-	// notice: 此处通过过滤 padding + RawStdEncoding 来兼容有/无padding的情况
-	cleanStr = strings.TrimRight(cleanStr, string(base64.StdPadding))
-	ret, err := base64.RawStdEncoding.DecodeString(cleanStr)
+	// notice: padding 以被过滤，此处使用 RawStdEncoding 而非 StdEncoding
+	ret, err := base64.RawStdEncoding.DecodeString(str)
 	if err != nil {
 		return "", false
 	}
 	return string(ret), true
 }
-func base64CleanStr(str string, strict bool) (string, bool) {
+func base64CleanStrWeak(str string) string {
 	var buf strings.Builder
+	buf.Grow(len(str))
 	for _, c := range []byte(str) {
-		// todo 此处可优化为 mask 数组 (参考 strings.asciiSet)
 		if strings.IndexByte(base64Table, c) >= 0 {
 			buf.WriteByte(c)
-		} else if c == ' ' {
-			// skip spaces
-		} else if strict {
+		}
+	}
+	str = buf.String()
+
+	if len(str)%4 == 1 {
+		str = str[:len(str)-1]
+	}
+	return str
+}
+
+func base64CleanStrStrict(str string) (string, bool) {
+	var buf strings.Builder
+
+	var padding int
+	for _, c := range []byte(str) {
+		if c == base64padding {
+			padding++
+			continue
+		}
+
+		/* skip whitespace */
+		if strings.IndexByte("\t\r\n ", c) >= 0 {
+			continue
+		}
+		/* fail if any data follows padding */
+		if padding > 0 {
+			return "", false
+		}
+		if strings.IndexByte(base64Table, c) >= 0 {
+			buf.WriteByte(c)
+		} else {
+			/* fail on bad characters */
 			return "", false
 		}
 	}
-	return buf.String(), true
+
+	str = buf.String()
+
+	/* fail if the input is truncated (only one char in last group) */
+	if len(str)%4 == 1 {
+		return "", false
+	}
+
+	/* fail if the padding length is wrong (not VV==, VVV=), but accept zero padding
+	 * RFC 4648: "In some circumstances, the use of padding [--] is not required" */
+	if padding > 0 && (padding > 2 || (len(str)+padding)%4 != 0) {
+		return "", false
+	}
+
+	return str, true
 }
 
 func ZifBase64Encode(str string) string {
 	return PhpBase64Encode(str)
 }
 func ZifBase64Decode(str string, _ zpp.Opt, strict bool) (string, bool) {
-	return PhpBase64DecodeEx(str, strict)
+	return PhpBase64Decode(str, strict)
 }
--- ext/standard/base64_test.go
+++ ext/standard/base64_test.go
@@ -1,23 +1,133 @@
 package standard
 
-import "testing"
+import (
+	"encoding/hex"
+	"testing"
+)
 
 func TestPhpBase64Decode(t *testing.T) {
-	type args struct {
+	tests := []struct {
+		name   string
 		str    string
 		strict bool
-	}
-	tests := []struct {
-		name  string
-		args  args
-		want  string
-		want1 bool
+		want   string
+		want1  bool
 	}{
-		// TODO: Add test cases.
+		// base
+		{"base-0", "", false, "", true},
+		{"base-1", "a", false, "", true},
+		{"base-2", "aa", false, "69", true},
+		{"base-3", "aaa", false, "69a6", true},
+		{"base-4", "aaaa", false, "69a69a", true},
+		{"base-5", "aaaaa", false, "69a69a", true},
+		{"base-6", "aaaaaa", false, "69a69a69", true},
+		{"base-7", "aaaaaaa", false, "69a69a69a6", true},
+		{"base-8", "aaaaaaaa", false, "69a69a69a69a", true},
+		{"base-9", "aaaaaaaaa", false, "69a69a69a69a", true},
+		// padding
+		{"padding-0", "=", false, "", true},
+		{"padding-1", "a=", false, "", true},
+		{"padding-2", "aa=", false, "69", true},
+		{"padding-3", "aaa=", false, "69a6", true},
+		{"padding-4", "aaaa=", false, "69a69a", true},
+		{"padding-5", "aaaaa=", false, "69a69a", true},
+		{"padding-6", "aaaaaa=", false, "69a69a69", true},
+		{"padding-7", "aaaaaaa=", false, "69a69a69a6", true},
+		{"padding-8", "aaaaaaaa=", false, "69a69a69a69a", true},
+		{"padding-9", "aaaaaaaaa=", false, "69a69a69a69a", true},
+		// space
+		{"space-0", " a\r \ta=", false, "69", true},
+		{"space-1", "a a\r \ta=", false, "69a6", true},
+		{"space-2", "aa a\r \ta=", false, "69a69a", true},
+		{"space-3", "aaa a\r \ta=", false, "69a69a", true},
+		{"space-4", "aaaa a\r \ta=", false, "69a69a69", true},
+		{"space-5", "aaaaa a\r \ta=", false, "69a69a69a6", true},
+		{"space-6", "aaaaaa a\r \ta=", false, "69a69a69a69a", true},
+		{"space-7", "aaaaaaa a\r \ta=", false, "69a69a69a69a", true},
+		{"space-8", "aaaaaaaa a\r \ta=", false, "69a69a69a69a69", true},
+		{"space-9", "aaaaaaaaa a\r \ta=", false, "69a69a69a69a69a6", true},
+		// space-in-padding
+		{"space-in-padding-0", "= =", false, "", true},
+		{"space-in-padding-1", "a= =", false, "", true},
+		{"space-in-padding-2", "aa= =", false, "69", true},
+		{"space-in-padding-3", "aaa= =", false, "69a6", true},
+		{"space-in-padding-4", "aaaa= =", false, "69a69a", true},
+		{"space-in-padding-5", "aaaaa= =", false, "69a69a", true},
+		{"space-in-padding-6", "aaaaaa= =", false, "69a69a69", true},
+		{"space-in-padding-7", "aaaaaaa= =", false, "69a69a69a6", true},
+		{"space-in-padding-8", "aaaaaaaa= =", false, "69a69a69a69a", true},
+		{"space-in-padding-9", "aaaaaaaaa= =", false, "69a69a69a69a", true},
+		// padding-in-word
+		{"padding-in-word-0", "=aaaaa", false, "69a69a", true},
+		{"padding-in-word-1", "a=aaaaa", false, "69a69a69", true},
+		{"padding-in-word-2", "aa=aaaaa", false, "69a69a69a6", true},
+		{"padding-in-word-3", "aaa=aaaaa", false, "69a69a69a69a", true},
+		{"padding-in-word-4", "aaaa=aaaaa", false, "69a69a69a69a", true},
+		{"padding-in-word-5", "aaaaa=aaaaa", false, "69a69a69a69a69", true},
+		{"padding-in-word-6", "aaaaaa=aaaaa", false, "69a69a69a69a69a6", true},
+		{"padding-in-word-7", "aaaaaaa=aaaaa", false, "69a69a69a69a69a69a", true},
+		{"padding-in-word-8", "aaaaaaaa=aaaaa", false, "69a69a69a69a69a69a", true},
+		{"padding-in-word-9", "aaaaaaaaa=aaaaa", false, "69a69a69a69a69a69a69", true},
+		// strict-base
+		{"strict-base-0", "", true, "", true},
+		{"strict-base-1", "a", true, "", false},
+		{"strict-base-2", "aa", true, "69", true},
+		{"strict-base-3", "aaa", true, "69a6", true},
+		{"strict-base-4", "aaaa", true, "69a69a", true},
+		{"strict-base-5", "aaaaa", true, "", false},
+		{"strict-base-6", "aaaaaa", true, "69a69a69", true},
+		{"strict-base-7", "aaaaaaa", true, "69a69a69a6", true},
+		{"strict-base-8", "aaaaaaaa", true, "69a69a69a69a", true},
+		{"strict-base-9", "aaaaaaaaa", true, "", false},
+		// strict-padding
+		{"strict-padding-0", "=", true, "", false},
+		{"strict-padding-1", "a=", true, "", false},
+		{"strict-padding-2", "aa=", true, "", false},
+		{"strict-padding-3", "aaa=", true, "69a6", true},
+		{"strict-padding-4", "aaaa=", true, "", false},
+		{"strict-padding-5", "aaaaa=", true, "", false},
+		{"strict-padding-6", "aaaaaa=", true, "", false},
+		{"strict-padding-7", "aaaaaaa=", true, "69a69a69a6", true},
+		{"strict-padding-8", "aaaaaaaa=", true, "", false},
+		{"strict-padding-9", "aaaaaaaaa=", true, "", false},
+		// strict-space
+		{"strict-space-0", " a\r \ta=", true, "", false},
+		{"strict-space-1", "a a\r \ta=", true, "69a6", true},
+		{"strict-space-2", "aa a\r \ta=", true, "", false},
+		{"strict-space-3", "aaa a\r \ta=", true, "", false},
+		{"strict-space-4", "aaaa a\r \ta=", true, "", false},
+		{"strict-space-5", "aaaaa a\r \ta=", true, "69a69a69a6", true},
+		{"strict-space-6", "aaaaaa a\r \ta=", true, "", false},
+		{"strict-space-7", "aaaaaaa a\r \ta=", true, "", false},
+		{"strict-space-8", "aaaaaaaa a\r \ta=", true, "", false},
+		{"strict-space-9", "aaaaaaaaa a\r \ta=", true, "69a69a69a69a69a6", true},
+		// strict-space-in-padding
+		{"strict-space-in-padding-0", "= =", true, "", false},
+		{"strict-space-in-padding-1", "a= =", true, "", false},
+		{"strict-space-in-padding-2", "aa= =", true, "69", true},
+		{"strict-space-in-padding-3", "aaa= =", true, "", false},
+		{"strict-space-in-padding-4", "aaaa= =", true, "", false},
+		{"strict-space-in-padding-5", "aaaaa= =", true, "", false},
+		{"strict-space-in-padding-6", "aaaaaa= =", true, "69a69a69", true},
+		{"strict-space-in-padding-7", "aaaaaaa= =", true, "", false},
+		{"strict-space-in-padding-8", "aaaaaaaa= =", true, "", false},
+		{"strict-space-in-padding-9", "aaaaaaaaa= =", true, "", false},
+		// strict-padding-in-word
+		{"strict-padding-in-word-0", "=aaaaa", true, "", false},
+		{"strict-padding-in-word-1", "a=aaaaa", true, "", false},
+		{"strict-padding-in-word-2", "aa=aaaaa", true, "", false},
+		{"strict-padding-in-word-3", "aaa=aaaaa", true, "", false},
+		{"strict-padding-in-word-4", "aaaa=aaaaa", true, "", false},
+		{"strict-padding-in-word-5", "aaaaa=aaaaa", true, "", false},
+		{"strict-padding-in-word-6", "aaaaaa=aaaaa", true, "", false},
+		{"strict-padding-in-word-7", "aaaaaaa=aaaaa", true, "", false},
+		{"strict-padding-in-word-8", "aaaaaaaa=aaaaa", true, "", false},
+		{"strict-padding-in-word-9", "aaaaaaaaa=aaaaa", true, "", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := PhpBase64Decode(tt.args.str, tt.args.strict)
+			got, got1 := PhpBase64Decode(tt.str, tt.strict)
+			got = hex.EncodeToString([]byte(got))
 			if got != tt.want {
 				t.Errorf("PhpBase64Decode() got = %v, want %v", got, tt.want)
 			}
